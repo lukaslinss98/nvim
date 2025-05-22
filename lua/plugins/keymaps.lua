1d1c36b40e26c735b0980c3a7772d268a8f731e3
Lukas Linss <lukas.linss@maibornwolff.de>
2025-05-22 08:40:51 +0200

    add git symboles to nvim-tree and change nvim-tree keymaps

--- lua/plugins/keymaps.lua
+++ lua/plugins/keymaps.lua
@@ -1,12 +1,11 @@
 -- telescope
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>fs', builtin.git_files, {desc = 'Telescope find git files'})
+vim.keymap.set('n', '<leader>fs', builtin.git_files, { desc = 'Telescope find git files' })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 
 -- nvim.tree
-vim.keymap.set("n", "<leader>eo", ":NvimTreeOpen<cr>")
-vim.keymap.set("n", "<leader>c", ":NvimTreeClose<cr>")
+vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>', { noremap = true })
 
--- fine command-line
-vim.api.nvim_set_keymap('n', ':', '<cmd>FineCmdline<CR>', {noremap = true})
+--cmdline
+vim.api.nvim_set_keymap('n', ':', '<cmd>FineCmdline<CR>', { noremap = true })
--- lua/plugins/nvimtree.lua
+++ lua/plugins/nvimtree.lua
@@ -1,1 +1,16 @@
-require("nvim-tree").setup {}
+vim.opt.termguicolors = true
+
+require("nvim-tree").setup({
+    git = { enable = true },
+    renderer = {
+        highlight_git = true,
+        icons = {
+            show = {
+                git = true,
+            },
+        },
+    },
+    view = {
+        side = "left",
+    },
+})
