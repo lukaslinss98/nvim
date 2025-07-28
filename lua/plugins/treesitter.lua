29526243fbcb87094b1b6d8625f09533e92420e5
Lukas Linss <lukas.linss@maibornwolff.de>
2025-07-28 16:27:11 +0100

    add markdown to treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,20 +1,21 @@
 require("nvim-treesitter.configs").setup({
-    ensure_installed = {
-        "lua",
-        "javascript",
-        "typescript",
-        "python",
-        "go",
-        "html",
-        "css",
-        "bash",
-        "java",
-        "kotlin"
-    },                 -- Add the languages you use
-    highlight = {
-        enable = true, -- Enable syntax highlighting
-    },
-    indent = {
-        enable = true, -- Enable Tree-sitter-based indentation
-    },
+  ensure_installed = {
+    "lua",
+    "javascript",
+    "typescript",
+    "python",
+    "go",
+    "html",
+    "css",
+    "bash",
+    "java",
+    "kotlin",
+    "markdown"
+  },                   -- Add the languages you use
+  highlight = {
+    enable = true,     -- Enable syntax highlighting
+  },
+  indent = {
+    enable = true,     -- Enable Tree-sitter-based indentation
+  },
 })
